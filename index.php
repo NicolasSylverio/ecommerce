050614b91243c0674638838b68aa797fe848aa2a
NICOLAS SYLVERIO PEREIRA <nicolas.pereira@fatec.sp.gov.br>
2018-11-19 18:03:09 -0200

    Adicionado a telas de AdminLte e a tela de login de usuario adm

--- index.php
+++ index.php
@@ -1,25 +1,60 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Fatec\Page; 
+use \Fatec\PageAdmin;
+use \Fatec\Model\User;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Fatec\Page();
+	$page = new Page();
 
 	$page->setTpl("index");
 
 });
 
 $app->get('/admin', function() {
-    
-	$page = new Fatec\PageAdmin();
+	
+	User::verifyLogin();
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+		]);
+
+	$page->setTpl("login");	
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["senha"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
