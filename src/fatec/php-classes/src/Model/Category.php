c261ec5dceca7c1135aa1ca7e35f87f18379806c
NicolasSylverio <nicolas_sylveriopereira@hotmail.com>
2018-11-27 22:54:14 -0200

    Adicionado Cadastro de Categorias

---
+++ src/fatec/php-classes/src/Model/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Fatec\Model;
+
+use \Fatec\DB\Sql;
+use \Fatec\Model;
+use \Fatec\Mailer;
+
+    class Category extends Model{
+
+        public static function listAll()
+        {
+            $sql = new Sql();
+
+            return $sql->select("SELECT * FROM tb_categories  ORDER BY descategory");
+        }
+
+        public function save()
+        {
+            $sql = new Sql();
+
+            $result = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
+                    ":idcategory"=>$this->getidcategory(),
+                    ":descategory"=>$this->getdescategory(),
+                ));
+
+            $this->setData($result[0]);
+        }
+
+        public function get($idcategory)
+        {
+            $sql = new Sql();
+
+            $result = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+                ":idcategory"=>$idcategory
+            ]);
+
+            $this->setData($result[0]);
+        }
+
+        public function delete()
+        {
+            $sql = new Sql();
+
+            $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
+                ":idcategory"=>$this->getidcategory()
+            ]);
+        }
+
+        public function update()
+        {
+            $sql = new Sql();
+
+            $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
+                ":idcategory"=>$this->getidcategory()
+            ]);
+        }
+    }
+?>
